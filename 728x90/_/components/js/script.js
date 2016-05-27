fa790acd0b2b3d173e690dad51ba65a532d97841
joshuacitrak <joshuacitrak@gmail.com>
2016-05-27 15:06:02 -0700

    final sync

--- 728x90/_/components/js/script.js
+++ 728x90/_/components/js/script.js
@@ -66,44 +66,6 @@ function checkInit() {
 
 window.addEventListener('load', checkInit);
 
-var tl = new TimelineLite({onUpdate:updateSlider});
+var tl = new TimelineLite();
 tl.eventCallback("onComplete", animationComplete);
 
-
-$("#play").click(function() {
-  //play() only plays forward from current position. If timeline has finished, play() has nowhere to go.
-  //if the timeline is not done then play() or else restart() (restart always restarts from the beginning).
-
-  if(tl.progress() != 1){
-    //carl just changed this again
-		tl.play();
-  } else {
-    tl.restart();
-  }
-});
-		
-$("#pause").click(function() {
-		tl.pause();
-});
-		
-$("#restart").click(function() {
-		tl.restart();
-});		
-	
-$("#slider").slider({
-  range: false,
-  min: 0,
-  max: 100,
-  step:.1,
-  slide: function ( event, ui ) {
-    tl.pause();
-    //adjust the timeline's progress() based on slider value
-    tl.progress( ui.value/100 );
-    }
-});	
-		
-function updateSlider() {
-  $("#slider").slider("value", tl.progress() *100);
-} 	
-			  
-	
